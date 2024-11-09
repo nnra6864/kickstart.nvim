961b795adbdd4df3b8f38f9ebbe21feb862baa8a
nnra6864 <enenragd@gmail.com>
2024-11-09 10:50:50 +0100

    Added NERDTree

--- init.lua
+++ init.lua
@@ -829,6 +829,19 @@ require('lazy').setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
+
+  -- Nerd Tree
+  {
+    'preservim/nerdtree',
+    config = function()
+      -- Key mappings for NERDTree
+      vim.api.nvim_set_keymap('n', '<leader>n', ':NERDTreeFocus<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<C-n>', ':NERDTree<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<C-f>', ':NERDTreeFind<CR>', { noremap = true, silent = true })
+    end,
+  },
+
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
