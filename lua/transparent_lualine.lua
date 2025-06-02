546153d1ad7af00b086ec205177397f8289fba5c
nnra6864 <enenragd@gmail.com>
2025-06-02 16:24:55 +0200

    Switched to Frost

--- lua/transparent_lualine.lua
+++ lua/transparent_lualine.lua
@@ -2,34 +2,34 @@ local M = {}
 M.theme = function()
 	return {
 		inactive = {
-			a = { fg = "#B2B2B2", bg = "#080808", gui = "bold" },
-			b = { fg = "#B2B2B2", bg = "#080808" },
-			c = { fg = "#B2B2B2", bg = nil },
+			a = { fg = "#ACCAE6", bg = "#102840", gui = "bold" },
+			b = { fg = "#ACCAE6", bg = "#102840" },
+			c = { fg = "#ACCAE6", bg = nil },
 		},
 		visual = {
-			a = { fg = "#B2B2B2", bg = "#006699", gui = "bold" },
-			b = { fg = "#B2B2B2", bg = "#080808" },
-			c = { fg = "#B2B2B2", bg = nil },
+			a = { fg = "#ACCAE6", bg = "#266099", gui = "bold" },
+			b = { fg = "#ACCAE6", bg = "#102840" },
+			c = { fg = "#ACCAE6", bg = nil },
 		},
 		replace = {
-			a = { fg = "#B2B2B2", bg = "#990000", gui = "bold" },
-			b = { fg = "#B2B2B2", bg = "#080808" },
-			c = { fg = "#B2B2B2", bg = nil },
+			a = { fg = "#ACCAE6", bg = "#13304D", gui = "bold" },
+			b = { fg = "#ACCAE6", bg = "#102840" },
+			c = { fg = "#ACCAE6", bg = nil },
 		},
 		normal = {
-			a = { fg = "#B2B2B2", bg = "#080808", gui = "bold" },
-			b = { fg = "#B2B2B2", bg = "#080808" },
-			c = { fg = "#B2B2B2", bg = nil },
+			a = { fg = "#ACCAE6", bg = "#102840", gui = "bold" },
+			b = { fg = "#ACCAE6", bg = "#102840" },
+			c = { fg = "#ACCAE6", bg = nil },
 		},
 		insert = {
-			a = { fg = "#080808", bg = "#00990D", gui = "bold" },
-			b = { fg = "#B2B2B2", bg = "#080808" },
-			c = { fg = "#B2B2B2", bg = nil },
+			a = { fg = "#102840", bg = "#1A4066", gui = "bold" },
+			b = { fg = "#ACCAE6", bg = "#102840" },
+			c = { fg = "#ACCAE6", bg = nil },
 		},
 		command = {
-			a = { fg = "#080808", bg = "#998000", gui = "bold" },
-			b = { fg = "#B2B2B2", bg = "#080808" },
-			c = { fg = "#B2B2B2", bg = nil },
+			a = { fg = "#102840", bg = "#205080", gui = "bold" },
+			b = { fg = "#ACCAE6", bg = "#102840" },
+			c = { fg = "#ACCAE6", bg = nil },
 		},
 	}
 end
