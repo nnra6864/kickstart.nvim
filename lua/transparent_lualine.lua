d7c71affe4291fcec9f2a493162e1b516a030c4a
nnra6864 <nnra2210@gmail.com>
2026-03-03 23:21:17 +0100

    ricer bs

--- lua/transparent_lualine.lua
+++ lua/transparent_lualine.lua
@@ -2,34 +2,34 @@ local M = {}
 M.theme = function()
 	return {
 		inactive = {
-			a = { fg = "#ACCAE6", bg = "#102840", gui = "bold" },
-			b = { fg = "#ACCAE6", bg = "#102840" },
-			c = { fg = "#ACCAE6", bg = nil },
+			a = { fg = "#D8DED3", bg = "#4C5843", gui = "bold" },
+			b = { fg = "#D8DED3", bg = "#5A6A50" },
+			c = { fg = "#D8DED3", bg = nil },
 		},
 		visual = {
-			a = { fg = "#ACCAE6", bg = "#3078BF", gui = "bold" },
-			b = { fg = "#ACCAE6", bg = "#102840" },
-			c = { fg = "#ACCAE6", bg = nil },
+			a = { fg = "#D8DED3", bg = "#C4B550", gui = "bold" },
+			b = { fg = "#D8DED3", bg = "#5A6A50" },
+			c = { fg = "#D8DED3", bg = nil },
 		},
 		replace = {
-			a = { fg = "#ACCAE6", bg = "#BF3030", gui = "bold" },
-			b = { fg = "#ACCAE6", bg = "#102840" },
-			c = { fg = "#ACCAE6", bg = nil },
+			a = { fg = "#D8DED3", bg = "#4C5844", gui = "bold" },
+			b = { fg = "#D8DED3", bg = "#5A6A50" },
+			c = { fg = "#D8DED3", bg = nil },
 		},
 		normal = {
-			a = { fg = "#ACCAE6", bg = "#102840", gui = "bold" },
-			b = { fg = "#ACCAE6", bg = "#102840" },
-			c = { fg = "#ACCAE6", bg = nil },
+			a = { fg = "#D8DED3", bg = "#4C5843", gui = "bold" },
+			b = { fg = "#D8DED3", bg = "#5A6A50" },
+			c = { fg = "#D8DED3", bg = nil },
 		},
 		insert = {
-			a = { fg = "#102840", bg = "#60BF30", gui = "bold" },
-			b = { fg = "#ACCAE6", bg = "#102840" },
-			c = { fg = "#ACCAE6", bg = nil },
+			a = { fg = "#4C5843", bg = "#5A6A50", gui = "bold" },
+			b = { fg = "#D8DED3", bg = "#5A6A50" },
+			c = { fg = "#D8DED3", bg = nil },
 		},
 		command = {
-			a = { fg = "#102840", bg = "#BFA730", gui = "bold" },
-			b = { fg = "#ACCAE6", bg = "#102840" },
-			c = { fg = "#ACCAE6", bg = nil },
+			a = { fg = "#4C5843", bg = "#958831", gui = "bold" },
+			b = { fg = "#D8DED3", bg = "#5A6A50" },
+			c = { fg = "#D8DED3", bg = nil },
 		},
 	}
 end
