ad5ea32da9bd06cb1f9853cf7ed274bb2f4e4382
nnra6864 <nnra2210@gmail.com>
2026-03-28 19:27:01 +0100

    Added cs snippets

--- lua/plugins/LuaSnip.lua
+++ lua/plugins/LuaSnip.lua
@@ -13,8 +13,14 @@ return {
   dependencies = {
     {
       'rafamadriz/friendly-snippets',
-      config = function() require('luasnip.loaders.from_vscode').lazy_load() end,
     },
   },
+  config = function()
+    local ls = require 'luasnip'
+    require('luasnip.loaders.from_vscode').lazy_load()
+    require('luasnip.loaders.from_lua').lazy_load {
+      paths = { vim.fn.stdpath 'config' .. '/lua/snippets' },
+    }
+  end,
   opts = {},
 }
